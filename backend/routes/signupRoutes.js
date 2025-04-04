beeebb3664bf5cd8ada0379bf33cec5101d3dd50
ahmed gacem <116039292+ahmedchnaider@users.noreply.github.com>
2025-04-04 03:20:42 +0200

    Delete backend directory

--- backend/routes/signupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { signup } = require('../controllers/signupController');
-const router = express.Router();
-
-// Route to handle signup
-router.post('/', signup);
-
-module.exports = router;
--- backend/routes/tixieaRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { makeCall } = require('../controllers/tixieaController');
-
-router.post('/call', makeCall);
-
-module.exports = router; 
